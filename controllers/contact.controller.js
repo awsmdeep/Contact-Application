d8c818bd92ce532b6fd28a41e3ac55f18d61bc0e
deepak <deepakdas538@gmail.com>
2024-09-10 21:26:52 +0530

    completed

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -2,7 +2,7 @@ import asyncHandler from "express-async-handler";
 import { Contact } from "../models/contact.model.js";
 
 export const getAllContacts = asyncHandler(async (req, res) => {
-  const contacts = await Contact.find();
+  const contacts = await Contact.find({user_id:req.user.id});
   res.status(200).json(contacts);
 });
 
@@ -26,6 +26,7 @@ export const createContact = asyncHandler(async (req, res) => {
     name,
     email,
     phoneNumber,
+    user_id:req.user.id
   });
 
   res.status(201).json(contact);
@@ -36,6 +37,10 @@ export const updateContact = asyncHandler(async (req, res) => {
   if (!contact) {
     res.status(404);
     throw new Error("contact not found");
+  }
+  if(contact.user_id.toString()!==req.user.id){
+    res.status(403);
+    throw new Error("user dont have permission to update other user contacts")
   }
   const updatedContact = await Contact.findByIdAndUpdate(
     req.params.id,
@@ -50,6 +55,10 @@ export const deleteContact = asyncHandler(async (req, res) => {
   if (!contact) {
     res.status(404);
     throw new Error("contact not found");
+  }
+  if(contact.user_id.toString()!==req.user.id){
+    res.status(403);
+    throw new Error("user dont have permission to delete other user contacts")
   }
   await Contact.findByIdAndDelete(req.params.id);
   res.status(200).json({ message: `deleted contact ${req.body.name}` });
--- routes/contact.route.js
+++ routes/contact.route.js
@@ -1,6 +1,9 @@
 import express from "express"
+import { validToken } from "../middleware/validateToken.js"
 const router =express.Router();
 import { getAllContacts,getOneContact,updateContact,deleteContact,createContact } from "../controllers/contact.controller.js";
+
+router.use(validToken)
 
 router.get("/",getAllContacts)
 router.get("/:id",getOneContact)
